5374238d8585f61efae45acbc84d446ce1fca918
yuvansankar-dev <148607533+yuvansankar-dev@users.noreply.github.com>
2024-03-13 18:52:55 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,38 @@
+import express from "express";
+import axios from "axios";
+import cors from "cors";
+
+const app = express();
+const port = 3000;
+
+const corsOptions = {
+  origin: "http://127.0.0.1:5173",
+  methods: ["GET", "POST"],
+  allowedHeaders: ["Content-Type"],
+  credentials: true,
+};
+app.use(cors(corsOptions));
+app.use(express.json());
+
+app.post("/sendData", async (req, res) => {
+  console.log("start");
+  axios
+    .post(
+      "https://webhook.site/c7ecffc1-ceaf-4937-967d-b915581003fe/",
+      req.body
+    )
+    .then((response) => {
+      console.log("success");
+      res.send(response.data);
+    })
+    .catch((error) => {
+      console.error("Error sending data:", error);
+      res.status(500).json({
+        error: "An error occurred while sending data to Webhook.site.",
+      });
+    });
+});
+
+app.listen(port, () => {
+  console.log(`Server running at http://localhost:${port}`);
+});
